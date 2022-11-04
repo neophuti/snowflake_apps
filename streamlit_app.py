8a642a59ea42bf342b977b12634d2b360739fec9
Lehlohonolo Neo Phuti <106586494+neophuti@users.noreply.github.com>
2022-11-04 17:44:54 +0200

    Update streamlit_app.py
    Load and display fruits from the multi-select to the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,13 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 #Pick list for choosing which fruits the customer wnats to include in their smoothie and prepopulate
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#Load selected fruits into dataframe
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display fruits table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
 
