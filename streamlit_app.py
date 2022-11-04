50ed691598ce8d9041b0d7188b9be45bd23dcd29
Lehlohonolo Neo Phuti <106586494+neophuti@users.noreply.github.com>
2022-11-04 17:23:54 +0200

    Update streamlit_app.py
    Prepopulate multi-select list with some options (fruits) as an example to the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Load fruits file into dataframe
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Set index to the name of the fruit
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-#Pick list for choosing which fruits the customer wnats to include in their smoothie
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#Pick list for choosing which fruits the customer wnats to include in their smoothie and prepopulate
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #Display fruits table
 streamlit.dataframe(my_fruit_list)
