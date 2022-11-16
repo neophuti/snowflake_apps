43ebb71c124723ebd44084d73143df581b2f45e2
Lehlohonolo Neo Phuti <106586494+neophuti@users.noreply.github.com>
2022-11-16 13:43:05 +0200

    Update streamlit_app.py
    Added a header for the fruity vice service data
    Displaying response (JSON) data from the API service

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display fruits table
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display fruityvice API response
+streamlit.header('Fruityvice Fruit Advice!')
+
 #Call Fuityvice service, get fruit
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 #Display fruit
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
