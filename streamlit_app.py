b0b69a0371d224b7a9cd59324395428a54079d96
Lehlohonolo Neo Phuti <106586494+neophuti@users.noreply.github.com>
2022-11-16 15:02:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,11 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered, ' fruit_choice)
+
 #Call Fuityvice service, get fruit
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Take the JSON response and normalise it
 fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
